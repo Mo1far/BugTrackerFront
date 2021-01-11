226c1e44edc9d7d1add986a3f559adcecceb4411
vapelavsky <mscn.brtlmo@gmail.com>
2021-01-11 03:26:24 +0200

    Developed design of cards

--- src/components/Bug.js
+++ src/components/Bug.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Bug = ({bug}) => (
     <div className="card">
-        <div className="card-image"></div>
+        <div className="card-image" />
         <div className="card-text">
             <h2>Баг №{bug.id}</h2>
             <p>{bug.description}</p>
