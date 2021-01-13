bca9a158845ea6303df3afdddb21076555371df4
Mo1far <vladuslav09@gmail.com>
2021-01-13 22:47:40 +0200

    aga

--- src/components/Bug.js
+++ src/components/Bug.js
@@ -2,22 +2,21 @@ import React from "react";
 import "../styles/Bug.scss"
 
 const statuses = {
-    'pending': 'Зареєстровано',
-    'registered': 'Очікує рішення ',
+    'pending': 'Очікує рішення',
+    'registered': 'Зареєстровано',
     'fixed': 'Вирішено',
     'dropped': 'Відхилено',
 };
 
 export const Bug = ({bug}) => (
-<div className="card">
-    <div className="card_image">
-        <img src="https://elit-bugtracker.tk/uploads/bugs/24.jpg" alt={bug.description} className="card_image-img"/>
+    <div className="card">
+        <div className="card_image">
+            <img src={bug.photo_img} alt={bug.description} className="card_image-img"/>
+        </div>
+        <div className="card-text">
+            <h2>Баг №{bug.id}</h2>
+            <p>{bug.description}</p>
+            <div className={bug.status + " card-status"}>{statuses[bug.status]}</div>
+        </div>
     </div>
-    <div className="card-text">
-        <h2>Баг №{bug.id}</h2>
-        <p>{bug.description}</p>
-        <div className={bug.status + " card-status"}>{statuses[bug.status]}</div>
-    </div>
-</div>
 )
-
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -7,15 +7,33 @@ export const Categories = ({categories, toggleCategory}) => {
         <div className="categories">
             <div className="categories_block">
                 <ul className="categories_list">
-                    <li><input type="checkbox" id="checkboxOne" value="Rainbow Dash" defaultChecked={true}/><label
-                        htmlFor="checkboxOne">Вирішено</label></li>
-                    <li><input type="checkbox" id="checkboxTwo" value="Cotton Candy" defaultChecked={true}/><label
-                        htmlFor="checkboxTwo">Очікує
-                        рішення</label></li>
-                    <li><input type="checkbox" id="checkboxThree" value="Rarity" defaultChecked={true}/><label
-                        htmlFor="checkboxThree">Зареєстровано</label></li>
-                    <li><input type="checkbox" id="checkboxFour" value="Moondancer"/><label
-                        htmlFor="checkboxFour">Відхилено</label></li>
+                    <li><input type="checkbox"
+                               id="checkboxOne"
+                               value="Rainbow Dash"
+                               defaultChecked={true}
+                               onChange={() => toggleCategory("fixed")}/>
+                        <label htmlFor="checkboxOne">Вирішено</label>
+                    </li>
+                    <li><input type="checkbox"
+                               id="checkboxTwo"
+                               value="Cotton Candy"
+                               defaultChecked={true}
+                               onChange={() => toggleCategory("pending")}/>
+                        <label htmlFor="checkboxTwo">Очікує рішення</label>
+                    </li>
+                    <li><input type="checkbox"
+                               id="checkboxThree"
+                               value="Rarity"
+                               defaultChecked={true}
+                               onChange={() => toggleCategory("registered")}/>
+                        <label htmlFor="checkboxThree">Зареєстровано</label>
+                    </li>
+                    <li><input type="checkbox"
+                               id="checkboxFour"
+                               value="Moondancer"
+                               onChange={() => toggleCategory("dropped")}/>
+                        <label htmlFor="checkboxFour">Відхилено</label>
+                    </li>
                 </ul>
             </div>
 
