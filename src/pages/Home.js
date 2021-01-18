2b24e16b788310bf4e931fa53b54522822328050
Mo1far <vladuslav09@gmail.com>
2021-01-18 20:57:15 +0200

    aga

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,24 +3,52 @@ import { Bugs } from "../components/Bugs";
 import { ApiContext } from "../context/api/apiContext";
 import { Loader } from "../components/Loader";
 import { Categories } from "../components/Categories";
+import axios from "axios";
+import { ADD_BUGS } from "../context/types";
+import { apiReducer } from "../context/api/apiReducer";
+
+const url = process.env.REACT_APP_API_HOST;
+
+
+const HandleScroll = async () => {
+  const { state, dispatch, showLoader, categories, hideLoader } = useContext(
+    ApiContext
+  );
+
+  if (state.hasNext !== true) return;
+  if (
+    window.innerHeight + document.documentElement.scrollTop <=
+    document.documentElement.offsetHeight - 3
+  )
+    return;
+  console.log("Fetch more list items!");
+  state.page++;
+  showLoader();
+  let categoriesJ = JSON.stringify(categories);
+  const res = await axios.get(
+    `${url}/bugs?categories=${categoriesJ}&page=${state.page}&per_page=4`
+  );
+  const payload = state.bugs.concat(res.data.data);
+  if (payload.length === 0) {
+    state.hasNext = false;
+    hideLoader();
+  } else {
+    dispatch({ type: ADD_BUGS, payload });
+  }
+};
 
 export const Home = () => {
-  const {
-    loading,
-    bugs,
-    categories,
-    fetchBugs,
-    toggleCategory,
-    handleScroll,
-  } = useContext(ApiContext);
+  const { loading, bugs, categories, fetchBugs, toggleCategory } = useContext(
+    ApiContext
+  );
 
   useEffect(() => {
     fetchBugs();
   }, []);
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", (event) => handleScroll(event));
+    window.addEventListener("scroll", HandleScroll);
+    return () => window.removeEventListener("scroll", HandleScroll);
   }, []);
 
   return (
@@ -30,4 +58,4 @@ export const Home = () => {
       {loading ? <Loader /> : null}
     </Fragment>
   );
-}
+};
